95c3c5968a278d313b949237acaaab07a12a03ff
merveilalisa <merveilalisa@gmail.com>
2017-12-06 20:28:40 -0500

    first hafl
    home page and shopping list page

---
+++ src/app/firebase.export.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    var config = {
+        apiKey: "AIzaSyD7ANOW0i7ieUIFyBWDEA3-aF3-ShhIBCE",
+        authDomain: "shoppinglist-b764a.firebaseapp.com",
+        databaseURL: "https://shoppinglist-b764a.firebaseio.com",
+        projectId: "shoppinglist-b764a",
+        storageBucket: "",
+        messagingSenderId: "302353792540"
+}
